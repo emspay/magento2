acc42cab9ac0a2a190e15535e9e035c5180921c0
pirogandrej <pirog.andrej@gmail.com>
2019-10-17 11:50:03 +0300

    added magento-2 compatibility for validation csrf

--- app/code/EMS/Pay/Controller/Index/Ipn.php
+++ app/code/EMS/Pay/Controller/Index/Ipn.php
@@ -2,9 +2,27 @@
 
 namespace EMS\Pay\Controller\Index;
 
+use Magento\Framework\App\RequestInterface;
+
 if (interface_exists('Magento\Framework\App\CsrfAwareActionInterface')) {
-    class Ipn extends IpnCsrfCompatible
+    class Ipn extends IpnMain implements \Magento\Framework\App\CsrfAwareActionInterface
     {
+        /**
+         * @inheritDoc
+         */
+        public function createCsrfValidationException(
+            RequestInterface $request
+        ): ?\Magento\Framework\App\Request\InvalidRequestException {
+            return null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function validateForCsrf(RequestInterface $request): ?bool
+        {
+            return true;
+        }
     }
 } else {
     class Ipn extends IpnMain
